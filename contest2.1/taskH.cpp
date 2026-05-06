f79dc39ab188d76b2703751e33252fbe0ea11f2c
Никитин Арсений <125593020+sene4ga@users.noreply.github.com>
2026-05-06 17:57:25 +0300

    Create taskH.cpp

---
+++ contest2.1/taskH.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+  int n;
+  std::cin >> n;
+  long long ans = 0;
+
+  for (int a = 1; a <= n; ++a) {
+    int max_val = 2 * a - 1;
+    std::vector<long long> dp(n + 1, 0);
+    dp[0] = 1;
+    for (int x = a + 1; x <= max_val && x <= n; ++x) {
+      for (int s = n; s >= x; --s) {
+        dp[s] += dp[s - x];
+      }
+    }
+    if (n - a >= 0) {
+      ans += dp[n - a];
+    }
+  }
+  std::cout << ans << "\n";
+}
